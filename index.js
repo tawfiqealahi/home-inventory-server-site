11acd58ab9db2fcffafa5b89de3b518e3f2eb5bf
tawfiqealahi <tawfiqealahi99@gmail.com>
2022-02-23 19:34:24 -0600

    4

--- index.js
+++ index.js
@@ -4,10 +4,6 @@ const cors = require("cors");
 require("dotenv").config();
 const ObjectId = require("mongodb").ObjectId;
 
-
-// user: home-inventory
-// password: WtVvokfnw03UXyTj
-
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -15,23 +11,16 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// ki
-
-
-// const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jvpmw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, });
-// client.connect(err => {
-//   const collection = client.db("homeInventory").collection("users");
-//   // console.log('tawfiq ok ');
-//   client.close();
-// });
-
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 async function run() {
   try {
     await client.connect();
-    const database = client.db("homeInventory");
+    const database = client.db("niche_product");
     const productsCollection = database.collection("products");
     const orderCollection = database.collection("order");
     const reviewCollection = database.collection("review");
@@ -123,12 +112,12 @@ async function run() {
       res.send(updated);
     });
 
-
     //user data
     app.post("/userData", async (req, res) => {
       const user = await usersCollection.insertOne(req.body);
       res.send(user);
     });
+
 
     app.put("/makeAdmin", async (req, res) => {
       const filter = { email: req.body.email };
@@ -136,12 +125,14 @@ async function run() {
       if (result) {
         const newAdmin = await usersCollection.updateOne(filter, {
           $set: {
-            role: "admin",
+            role: "admin",  
           },
         });
         console.log(newAdmin);
+        
       }
     });
+
 
     //admin
     app.get("/admin/:email", async (req, res) => {
@@ -153,12 +144,12 @@ async function run() {
     //  await client.close();
   }
 }
+
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("home inventory Products Server Run");
+  res.send("home-inventory Products Server Run");
 });
-
 app.listen(port, () => {
-  console.log("home inventory product server at port", port);
+  console.log("home-inventory product server at port", port);
 });
